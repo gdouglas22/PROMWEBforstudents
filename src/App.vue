cf7440b2a1889cb605123c65ce8187e3b67b62a8
Bogdan <test@mail.com>
2023-12-17 13:00:45 +0300

    new version

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,59 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+  <div id="app">
+    <ContainerElement>
+      <ChatWindow @messagewassent="sendMessage" v-bind:messages="messages">
+        <ChatMessage v-for="message in messages" v-bind:key="message.id" v-bind:username="message.author" v-bind:datetime="message.datetime">{{ message.text }}</ChatMessage>
+      </ChatWindow>
+    </ContainerElement>
+  </div>
+  
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ContainerElement from './components/Container.vue'
+import ChatMessage from './components/ChatMessage.vue'
+import ChatWindow from './components/ChatWindow.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ContainerElement,
+    ChatMessage,
+    ChatWindow
+  },
+  data() {
+    return {
+      messages: [] // создаем массив для сообщений, которые мы будем получать из компонента ChatWindow
+    }
+  },
+  methods: {
+    getUserData() {
+      this.axios.get("https://657483b1b2fbb8f6509c4953.mockapi.io/chat/messages").then((response) => {
+        this.messages = response.data; // Обновляем массив сообщений
+      })
+        .catch((error) => {
+          console.log(error)
+        })
+        .then(() => { });
+    },
+    sendMessage(message) { // метод sendMessage получает объект message, из которого мы будем подставлять значения для нашего сообщения в чате
+      this.axios({
+        method: 'post',
+        url: 'https://657483b1b2fbb8f6509c4953.mockapi.io/chat/messages',
+        data: {
+          author: message.author,
+          text: message.text,
+        }
+      }).then((response) => { // выполняется в случае успешной отправки
+        console.log(response);
+        this.getUserData(); // После отправки сообщения обновляем сообщения снова
+      })
+    }
+  },
+  mounted() { // хук, чтобы запустить функцию в начале работы программы
+    this.getUserData();
   }
 }
 </script>
@@ -22,5 +66,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+body {
+  margin: 0;
+  background-color: #f9f9fa;
 }
 </style>
---
+++ src/components/Container.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container"><slot></slot></div>
+</template>
+
+<script>
+export default {
+  name: 'ContainerElement',
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.container {
+  width: 764px;
+  margin: auto;
+  box-shadow: 0 0 15px 0 #5f5f5f73;
+}
+</style>
